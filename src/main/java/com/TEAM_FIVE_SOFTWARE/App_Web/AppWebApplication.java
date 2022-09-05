2f929d97aa3a69aee259d0d18b13c8522ef220bd
Sebas Yepes <ingyepesc@gmail.com>
2022-09-04 21:03:55 -0500

    Se hizo el primer, hola Mundo, posteriormente creamos las 3 clases de nuestro proyecto, y se modelo la clase Empresa

--- src/main/java/com/TEAM_FIVE_SOFTWARE/App_Web/AppWebApplication.java
+++ src/main/java/com/TEAM_FIVE_SOFTWARE/App_Web/AppWebApplication.java
@@ -2,9 +2,25 @@ package com.TEAM_FIVE_SOFTWARE.App_Web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class AppWebApplication {
+
+	@GetMapping("/hello")
+	public String hello(){
+
+		return "Hola Mundo de la Programación!";
+	}
+	@GetMapping("/test")
+	public String test(){
+		Empresa emp = new Empresa("SOLAR SAS", "Calle la geta", "3213213211", "800212132-3");
+		emp.setNombre("SOLAR LTDA");
+		return emp.getNombre();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppWebApplication.class, args);
---
+++ src/main/java/com/TEAM_FIVE_SOFTWARE/App_Web/Empleado.java
@@ -0,0 +1,4 @@
+package com.TEAM_FIVE_SOFTWARE.App_Web;
+
+public class Empleado {
+}
